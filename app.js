6bd694b02a80b0b30ab7e18efbc7c1348b7620cc
OrganicEgg <kllopez3397@gmail.com>
2021-02-23 09:05:27 +0800

    converted to vue 3 composition API

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            rating: 0,
-            books: [
-                {
-                    title: "Powerhouse",
-                    author: "Kenya Case",
-                    show_author: true,
-                    rating: 2
-                },
-                {
-                    title: "The Final Empire",
-                    author: "Brandon Sanderson",
-                    show_author: true,
-                    rating: 4
-                },
-                {
-                    title: "OFF The Grid",
-                    author: "Luis Doe",
-                    show_author: false,
-                    rating: 3.5
-                }
-            ]
-        }
-    },
-    methods: {
-        changeTitle(title) {
+    setup () {
+        var rating = 0
+        var books = [
+            {
+                title: "Powerhouse",
+                author: "Kenya Case",
+                show_author: true,
+                rating: 2
+            },
+            {
+                title: "The Final Empire",
+                author: "Brandon Sanderson",
+                show_author: true,
+                rating: 4
+            },
+            {
+                title: "OFF The Grid",
+                author: "Luis Doe",
+                show_author: false,
+                rating: 3.5
+            }
+        ]
+
+        function changeTitle(title) {
             this.title = title
-        },
-    },
+        }
+
+        return {
+            rating,
+            books,
+            changeTitle
+        }
+    }
 })
 
 app.component('star-rating', VueStarRating.default)
